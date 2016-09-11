e5b2f0706ae61f4b79381c883d1ade399f26ff14
Aneesh Abhyankar <aneesh1993@users.noreply.github.com>
2016-09-10 21:41:21 -0400

    Update html_form_send1.php

--- html_form_send1.php
+++ html_form_send1.php
@@ -21,10 +21,10 @@
 	$mail->IsSMTP();
 
 	$mail->SMTPAuth = true; 					// enable SMTP authentication
-	$mail->SMTPSecure = "ssl"; 					//"tls"; // sets the prefix to the servier
+	$mail->SMTPSecure = "tls"; 					//"tls"; // sets the prefix to the servier
 
 	$mail->Host = "aspmx.l.google.com"    //"smtp.gmail.com"; 			// sets GMAIL as the SMTP server
-	$mail->Port = 25; 							// 465(ssl)---587(tls)set the SMTP port for the GMAIL server
+	$mail->Port = 587; 							// 465(ssl)---587(tls)set the SMTP port for the GMAIL server
 
 	$mail->Username = "portfoliowebsite1993@gmail.com"; 	// GMAIL username
 	$mail->Password = $password; 				// GMAIL password
